369c7ba8f0aa9503f7c9c9177e1baa58aa53ebed
Traveller Coder <guillermobukowski@gmail.com>
2025-04-28 09:31:22 -0300

    ejemplos de tipos de datos

--- app/intro.js
+++ app/intro.js
@@ -1,3 +1,20 @@
-var saludo = "Hola Mundo";
-
-console.log(saludo)
+"use strict";
+//Tipos de datos y como se traspilan de typescript a javascript
+let saludo = "Hola mundo"; //string
+let edad = 25; //number
+let esEstudiante = true; //boolean
+let fechaNacimiento = new Date(1998, 11, 25); //Date
+let listaDeNumeros = [1, 2, 3, 4, 5]; //Array<number>
+let listaDeNombres = ["Juan", "Pedro", "Maria"]; //Array<string>
+let colores = ["rojo", "verde", "azul"]; //Array<string>
+var diasDeLaSemana;
+(function (diasDeLaSemana) {
+    diasDeLaSemana[diasDeLaSemana["Lunes"] = 0] = "Lunes";
+    diasDeLaSemana[diasDeLaSemana["Martes"] = 1] = "Martes";
+    diasDeLaSemana[diasDeLaSemana["Miercoles"] = 2] = "Miercoles";
+    diasDeLaSemana[diasDeLaSemana["Jueves"] = 3] = "Jueves";
+    diasDeLaSemana[diasDeLaSemana["Viernes"] = 4] = "Viernes";
+    diasDeLaSemana[diasDeLaSemana["Sabado"] = 5] = "Sabado";
+    diasDeLaSemana[diasDeLaSemana["Domingo"] = 6] = "Domingo";
+})(diasDeLaSemana || (diasDeLaSemana = {}));
+let hoy = diasDeLaSemana.Lunes; //Enum
--- app/intro.ts
+++ app/intro.ts
@@ -1,3 +1,28 @@
-let saludo = "Hola Mundo";
+//Tipos de datos y como se traspilan de typescript a javascript
 
-console.log(saludo)
+
+let saludo : string = "Hola mundo"; //string
+
+let edad : number = 25; //number
+
+let esEstudiante : boolean = true; //boolean
+
+let fechaNacimiento : Date = new Date(1998, 11, 25); //Date
+
+let listaDeNumeros : number[] = [1, 2, 3, 4, 5]; //Array<number>
+
+let listaDeNombres : Array<string> = ["Juan", "Pedro", "Maria"]; //Array<string>
+
+let colores : string[] = ["rojo", "verde", "azul"]; //Array<string>
+
+enum diasDeLaSemana {
+    Lunes,
+    Martes, 
+    Miercoles, 
+    Jueves, 
+    Viernes,
+    Sabado,
+    Domingo 
+}
+
+let hoy : diasDeLaSemana = diasDeLaSemana.Lunes; //Enum
